1f7b9995f850afe09247425dac0bfa9f72665f61
dchen <dchen00@proton.me>
2026-05-03 12:59:23 +0200

    Initial: Spring initializer

---
+++ src/test/java/fr/natsystem/dchen/evaluation/EvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.natsystem.dchen.evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
